e4eaceefd1acb984979ff35f0075bf5cfcf38f8d
Joe Nunnelley <jcnunnelley@outlook.com>
2020-01-18 10:13:20 -0800

    lesson 8 activity: adding xss and csrf protections to this page

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
+import html
 import os
+import random
 import base64
 
-from flask import Flask, request
+from flask import Flask, request, session
 from model import Message 
 
 app = Flask(__name__)
+app.secret_key = b'_i67#y2L"F4Q8z\n\xec]/'
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    if 'csrf_token' not in session:
+      session['csrf_token'] = str(random.randint(1000000, 9999999))
 
     if request.method == 'POST':
-        m = Message(content=request.form['content'])
-        m.save()
+        if request.form.get('csrf_token', None) == session['csrf_token']:
+          m = Message(content=request.form['content'])
+          m.save()
 
     body = """
 <html>
@@ -21,17 +27,18 @@ def home():
 <form method="POST">
     <textarea name="content"></textarea>
     <input type="submit" value="Submit">
+    <input type="hidden" name="csrf_token" value="{}">
 </form>
 
 <h2>Wisdom From Your Fellow Classmates</h2>
-"""
+""".format(session['csrf_token'])
     
     for m in Message.select():
         body += """
 <div class="message">
 {}
 </div>
-""".format(m.content)
+""".format(html.escape(m.content))
 
     return body 
 
